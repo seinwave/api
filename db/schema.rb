905001b24e0cb8ab9b1bf029845b4fa6ed79f902
Matt Seidholz <mseidholz@gmail.com>
2023-10-06 15:23:24 -0400

    add category model, fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_192424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cultivar_groups", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
