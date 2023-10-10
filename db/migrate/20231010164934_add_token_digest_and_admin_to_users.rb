f730bb1bcd4a1b8dc0dd6221d028e56ea63b8b5d
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 12:50:21 -0400

    add token digest, admim to users

---
+++ db/migrate/20231010164934_add_token_digest_and_admin_to_users.rb
@@ -0,0 +1,6 @@
+class AddTokenDigestToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :admin, :boolean, default: false
+    add_column :users, :token_digest, :string
+  end
+end
