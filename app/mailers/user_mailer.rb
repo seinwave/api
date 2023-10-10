adfa9056e91d048a2f790db03e950f39592a330b
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 12:34:55 -0400

    add magic_link mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,7 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.magic_link.subject
-  #
   def magic_link
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = user
+    mail to: user.email, subject: "Here's your BBG Roses ✨Magic Link✨!"
   end
 end
