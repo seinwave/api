cd3f8471cb680c43d79985ba8e7dd20b0d7059be
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 11:51:15 -0400

    scaffold create session method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+
   def new
   end
+  
+  def create
+    sgid = params.require(:sgid)
+
+    user = GlobalID::Locator.locate_signed(sgid, for: 'login')
+  
+    if user && user.is_a?(User)
+      # handle successful login
+    else 
+      # handle redirection
+    end
+  end 
 end
