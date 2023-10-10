66b5db95e1728cea49ba32e924118b3eb231ade6
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 09:16:41 -0400

    add format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :first_name, presence: true, length: {maximum: 50}
   validates(:last_name, length: {maximum: 50})
-  validates(:email, presence: true, length: {maximum: 255})
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, 
+  format: {with: VALID_EMAIL_REGEX}
 end
