e525d5becf29a1f3b8ffb0bd4dc35a8b95b90433
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 09:11:20 -0400

    add presence validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates(:first_name, presence: true)
+  validates(:email, presence: true)
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(first_name: "Matt", last_name: "Example", email: "matt@example.com")
+  end 
+
+  test "should be valid" do
+    assert @user.valid?
+  end 
+
+  test "first_name should be present" do 
+    @user.first_name = " "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do 
+    @user.email = " "
+    assert_not @user.valid?
+  end 
 end
