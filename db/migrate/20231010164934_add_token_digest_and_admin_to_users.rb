021e49ae5dfa74de62d83cc5de81aa1524169cbe
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 12:50:56 -0400

    migrate admin and token digest

--- db/migrate/20231010164934_add_token_digest_and_admin_to_users.rb
+++ db/migrate/20231010164934_add_token_digest_and_admin_to_users.rb
@@ -1,4 +1,4 @@
-class AddTokenDigestToUsers < ActiveRecord::Migration[7.0]
+class AddTokenDigestAndAdminToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :admin, :boolean, default: false
     add_column :users, :token_digest, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_130618) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_164934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_130618) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.string "token_digest"
   end
 
   add_foreign_key "plants", "cultivars"
