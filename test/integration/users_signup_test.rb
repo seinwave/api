680c9475984e5d1ec3e521a7cf846cdb650a9ae9
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 13:03:54 -0400

    add failing test for signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,14 +13,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  # test "valid signup information" do
-  #   assert_difference 'User.count', 1 do
-  #     post users_path, params: { user: { name:  "Example User",
-  #                                        email: "user@example.com",
-  #                                        password:              "password",
-  #                                        password_confirmation: "password" } }
-  #   end
-  #   follow_redirect!
-  #   assert_template 'users/show'
-  # end
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",}}
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+
+  end
 end
