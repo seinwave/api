2e274ade802bc30b356bccabcbda435cb9668d1f
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 09:02:59 -0400

    add user controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup', to: "users#new"
+  resources :users 
   resources :cultivars
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   # root "articles#index"
 end
