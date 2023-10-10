48cfbab431800d1a297df42ff78b21fdbab752bd
Matt Seidholz <mseidholz@gmail.com>
2023-10-10 12:21:24 -0400

    handle failed signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # handle a successful save
+    else 
+      render 'new', status: :unprocessable_entity
+    end 
+  end
+  
+  private 
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,10 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post users_path, params: { user: { first_name:  "",
+                                         last_name: "bumpo",
                                          email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+                                           } }
     end
     assert_response :unprocessable_entity
     assert_template 'users/new'
