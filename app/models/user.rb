57402e8486c4b068835bee290b1dabdf8a7dfe91
Matt Seidholz <mseidholz@gmail.com>
2023-10-11 14:13:19 -0400

    add digest method

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,13 @@ class User < ApplicationRecord
     end
 
     def new_token
-        SecureRandom.urlsafe_base64
+      SecureRandom.urlsafe_base64
     end
+
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost:cost)
+    end  
 
   private
   def downcase_email
