dadd6a68b8501f3093e82f380b3786470d0e071f
Matt Seidholz <mseidholz@gmail.com>
2023-10-11 13:32:29 -0400

    add strong user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    puts params
+    
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
   end 
 end
