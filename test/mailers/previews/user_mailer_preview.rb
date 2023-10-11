5d17d468230a3f2ac0a35c6265a16a81573be331
Matt Seidholz <mseidholz@gmail.com>
2023-10-11 13:44:47 -0400

    generate magic_link mailer

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/magic_link
+  def magic_link
+    UserMailer.magic_link
+  end
+
+end
