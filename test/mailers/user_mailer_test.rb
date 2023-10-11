a2bc23c4fd69f51e9c16119ca116d488c23c413a
Matt Seidholz <mseidholz@gmail.com>
2023-10-11 14:25:42 -0400

    mailer set up, with no link

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,11 +4,11 @@ class UserMailerTest < ActionMailer::TestCase
   test "magic_link" do
     @user = users(:matt)
     mail = UserMailer.magic_link(@user)
-    assert_equal "Magic link", mail.subject
-    assert_equal [@user.email], mail.to
-    assert_equal ["from@example.com"],  mail.from
-    assert_match user.name,             mail.body.encoded
-    asser_match user.login_token,       mail.body.encoded
+    assert_equal "Magic link",           mail.subject
+    assert_equal [@user.email],          mail.to
+    assert_equal ["from@example.com"],   mail.from
+    assert_match @user.first_name,       mail.body.encoded
+    assert_match @user.login_token,      mail.body.encoded
     assert_match "Hi", mail.body.encoded
   end
 
