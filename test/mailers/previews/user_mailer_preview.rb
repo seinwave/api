d2505f08a6cfd0edc23147ec3c4710d9630c9e10
Matt Seidholz <mseidholz@gmail.com>
2023-10-12 11:32:14 -0400

    update mailer previews

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,9 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/magic_link
   def magic_link
-    UserMailer.magic_link
+    user = User.first 
+    user.login_token = User.new_token
+    UserMailer.magic_link(user)
   end
 
 end
