6bd4e3a309d8d9d0efd3dcbae787adb849f59c77
Matt Seidholz <mseidholz@gmail.com>
2023-10-12 09:40:30 -0400

    delete logout methods - well

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,23 +10,12 @@ module SessionsHelper
     !current_user.nil?
   end 
     
+  # generates tokens in the browser
   def remember(user)
     user.remember
     cookies.permanent.encrypted[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
     
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end 
-
-  def log_out
-    forget(current_user)
-    reset_session
-    @current_user = nil
-  end 
-
 
 end
