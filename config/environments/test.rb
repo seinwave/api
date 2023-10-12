3d5380fcb9793892b73e56374285b2ac47b85fe1
Matt Seidholz <mseidholz@gmail.com>
2023-10-12 10:20:46 -0400

    add default host urls to configs - test passing!

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,6 +8,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
+  
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
 
