5e10fbf644e0661b4ba63140a798fc0cfb3bc073
Matt Seidholz <mseidholz@gmail.com>
2023-10-26 14:31:41 -0400

    add favorites#update, call to api

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,14 @@
 class FavoritesController < ApplicationController
   before_action :logged_in_user
 
+  def update
+    cultivar = Cultivar.find(params[:favorite_cultivar_id])
+    respond_to do |format|
+      format.html { redirect_to '/map'}
+      format.turbo_stream
+    end
+  end 
+
   def create
     cultivar = Cultivar.find(params[:favorite_cultivar_id])
     current_user.favorite(cultivar)
--- app/javascript/controllers/map_controller/api.ts
+++ app/javascript/controllers/map_controller/api.ts
@@ -35,6 +35,36 @@ export async function routeToInfoPanel(cultivarId: number) {
 
     const body = await response.text();
     Turbo.renderStreamMessage(body);
+
+    await turboUpdateFavorites(cultivarId);
+  } catch (error) {
+    console.error('Error fetching cultivar info:', error);
+    throw error;
+  }
+}
+
+async function turboUpdateFavorites(cultivarId: number) {
+  const token = document && document.querySelector('meta[name="csrf-token"]');
+  if (!token) {
+    throw new Error('CSRF token not found');
+  }
+
+  // TODO: what endpoint should go here, to update the favorites view?
+  try {
+    const response = await fetch(`/favorites/${cultivarId}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'text/vnd.turbo-stream.html',
+        'X-CSRF-Token': token.getAttribute('content') || '',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const body = await response.text();
+    Turbo.renderStreamMessage(body);
   } catch (error) {
     console.error('Error fetching cultivar info:', error);
     throw error;
