015b67f4b9b18218d47b829bfced6611667da90b
Matt Seidholz <mseidholz@gmail.com>
2023-10-29 09:37:18 -0400

    format routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
   get     '/login',                         to: "sessions#new"
   delete  '/logout',                        to: "sessions#destroy"
   post    '/login',                         to: "sessions#create"
-  get     '/magic_link',                    to: "sessions#edit",         as: "magic_link"
+  get     '/magic_link',                    to: "sessions#edit",          as: "magic_link"
   get     '/map',                           to: "map#show"
   post    'map_data/info_panel/:id',        to: "info_panel#create"
-  post    'favorite_cultivar/:id',          to: "users#toggle_favorite", as: "favorite_cultivar"
+  post    'favorite_cultivar/:id',          to: "users#toggle_favorite",  as: "favorite_cultivar"
   post    'hide_info',                      to: "info_panel#hide",        as: "hide_info_panel"
   resources :users 
   resources :cultivars
   resources :favorites,                     only: [:create]
-  delete '/favorites/:id',                  to: 'favorites#destroy',     as: "delete_favorite"
+  delete '/favorites/:id',                  to: 'favorites#destroy',      as: "delete_favorite"
   root "cultivars#index"
 end
