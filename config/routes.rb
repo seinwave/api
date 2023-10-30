bbef8e85d473d83c411d55963881997c3edbabad
Matt Seidholz <mseidholz@gmail.com>
2023-10-30 13:08:19 -0400

    add integration test for map request

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post    '/login',                         to: "sessions#create"
   get     '/magic_link',                    to: "sessions#edit",          as: "magic_link"
   get     '/map',                           to: "map#show"
-  post    'map_data/info_panel/:id',        to: "info_panel#create"
+  post    'map_data/info_panel/:id',        to: "info_panel#create",      as: "info_panel"
   delete  'favorite_cultivar/:id',          to: "favorites#destroy",      as: "delete_favorite"
   post    'favorite_culitvar/:id',          to: "favorites#create",       as: "add_favorite"
   post    'hide_info',                      to: "info_panel#hide",        as: "hide_info_panel"
--- test/integration/map_test.rb
+++ test/integration/map_test.rb
@@ -6,4 +6,10 @@ class MapTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'map/show'
   end
+
+  test "a map_data request for a cultivar should reveal an info_panel with relevant information" do 
+    post info_panel_path(1, format: :turbo_stream)
+    assert_response :success
+    assert_template 'info_panel/_revealed'
+  end
 end
