e3d523910eb3b7f420de9677cb298780d3d61e31
Matt Seidholz <mseidholz@gmail.com>
2023-10-30 07:59:25 -0400

    re-add show method

--- app/controllers/cultivars_controller.rb
+++ app/controllers/cultivars_controller.rb
@@ -2,4 +2,12 @@ class CultivarsController < ApplicationController
   def index
     @cultivars = Cultivar.all
   end
+
+  def show
+    @cultivar = Cultivar.find(params[:id])
+    render json: @cultivar
+  end
+
+
+
 end
