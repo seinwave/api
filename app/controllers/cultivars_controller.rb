1b111374a1819f284086cc4c5577e8313694f6d4
Matt Seidholz <mseidholz@gmail.com>
2023-10-30 07:58:10 -0400

    remove extraneous controller methods from cultivars

--- app/controllers/cultivars_controller.rb
+++ app/controllers/cultivars_controller.rb
@@ -2,23 +2,4 @@ class CultivarsController < ApplicationController
   def index
     @cultivars = Cultivar.all
   end
-
-  # def show
-  #   @cultivar = Cultivar.find(params[:id])
-  #   set_current_cultivar(@cultivar)
-  #   render json: @cultivar
-  # end
-
-  # def hide
-  #   session[:cultivar_id] = nil
-  # end 
-
-  # def set_current_cultivar(cultivar)    
-  #   session[:cultivar_id] = cultivar.id
-  # end 
-
-  # def current_cultivar
-  #   return session[:cultivar_id] if session[:cultivar_id]
-  # end
-
 end
