97379917aef4b2fa2878f7f707482846e4e44efe
Matt Seidholz <mseidholz@gmail.com>
2023-10-31 09:43:21 -0400

    revert to form

--- app/javascript/controllers/search_controller/search_controller.class.ts
+++ app/javascript/controllers/search_controller/search_controller.class.ts
@@ -2,5 +2,10 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="search"
 export class SearchController extends Controller {
-  connect() {}
+  connect() {
+    const searchInput = document.getElementById('search-input');
+    searchInput?.addEventListener('keydown', async (event) => {
+      console.log(event);
+    });
+  }
 }
