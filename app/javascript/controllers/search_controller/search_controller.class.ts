34d224c0e999d4eb83a95159387cdd1a0fdfbb66
Matt Seidholz <mseidholz@gmail.com>
2023-10-31 09:54:21 -0400

    add working query - not sure about form data

--- app/javascript/controllers/search_controller/search_controller.class.ts
+++ app/javascript/controllers/search_controller/search_controller.class.ts
@@ -1,11 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
+import { queryCultivars } from './api';
 
 // Connects to data-controller="search"
 export class SearchController extends Controller {
-  connect() {
-    const searchInput = document.getElementById('search-input');
-    searchInput?.addEventListener('keydown', async (event) => {
-      console.log(event);
+  initialize() {
+    console.log('INITIALIZE SEARCH CONTROLLER');
+    const searchForm = document.getElementById('search-form');
+    const searchInput = document.getElementsByClassName(
+      'search-input'
+    )[0] as HTMLInputElement;
+
+    searchForm?.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const query = searchInput.value || '';
+      queryCultivars(query);
     });
   }
 }
