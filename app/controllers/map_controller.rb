b19336e8f5e66a003314813d68de999ba47cfe38
Matt Seidholz <mseidholz@gmail.com>
2023-10-31 08:51:45 -0400

    add query controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,4 +4,9 @@ class MapController < ApplicationController
       @current_cultivar = nil
   end
 
+  def query(query_string)
+    @result_plants = Plant.where("name LIKE ?", "%#{query_string}%")
+    render json: @result_plants
+  end
+
 end
--- test/controllers/map_controller_test.rb
+++ test/controllers/map_controller_test.rb
@@ -1,4 +1,18 @@
 require "test_helper"
 
 class MapControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get show" do
+    get "/map"
+    assert_response :success
+    assert_select "div#map-container"
+  end
+
+  test "query should return json" do
+    get "/map_data/plants?query=jude"
+    assert_response :success
+    assert_equal "application/json", @response.media_type
+    puts @response.body
+    assert_equal "[]", @response.body
+  end
 end
