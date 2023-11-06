4456f6dbd20e9ed9d718c21d91faf9173d0ea36d
Matt Seidholz <mseidholz@gmail.com>
2023-11-06 13:27:14 -0500

    add fetch cultivar names

--- app/javascript/controllers/search_controller/api.ts
+++ app/javascript/controllers/search_controller/api.ts
@@ -13,3 +13,17 @@ export async function queryCultivars(queryString: string): Promise<Cultivar[]> {
     throw error;
   }
 }
+
+export async function allCultivarNames(): Promise<String[]> {
+  try {
+    const response = await fetch(`/map_data/cultivars_names`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching cultivar names', error);
+    throw error;
+  }
+}
