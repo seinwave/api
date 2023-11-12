f328f59ab33c6166eca524e77397d9f4204f5850
Matt Seidholz <mseidholz@gmail.com>
2023-11-12 16:00:59 -0500

    add text labels

--- app/javascript/controllers/map_controller/map.ts
+++ app/javascript/controllers/map_controller/map.ts
@@ -64,6 +64,10 @@ function generateMarkers(map: Map) {
         'icon-image': 'rose-icon',
         'icon-size': ['interpolate', ['linear'], ['zoom'], 0, 0.005, 22, 0.05],
         'icon-allow-overlap': false,
+        'text-field': ['get', 'cultivar_name'],
+        'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+        'text-radial-offset': 1.5,
+        'text-justify': 'auto',
       },
     });
   });
