b16aa85f59f23a6dfec5889dc6fbae67768b5be5
Matt Seidholz <mseidholz@gmail.com>
2023-11-12 16:18:25 -0500

    add is_favorite attribute to returned plant object

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,15 @@ class PlantsController < ApplicationController
   def mapped_plants
     @plants_to_map = Plant.all.map do |plant|
       plant.attributes.merge(cultivar_name: Cultivar.find(plant.cultivar_id).name)
+      plant.attributes.merge(is_favorite: is_favorite(Cultivar.find(plant.cultivar_id)))
     end 
     render json: @plants_to_map
+  end
+
+  private 
+
+  def is_favorite(cultivar)?
+    return false if !current_user
+    return current_user.favorited?(cultivar)
   end
 end
