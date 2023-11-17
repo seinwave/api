017083a7a18c7082035cee5219f8b12a99e9437d
Matt Seidholz <mseidholz@gmail.com>
2023-11-17 14:24:37 -0500

    simplify routing problem - use bespoke views

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   post    '/login',                         to: "sessions#create"
   get     '/magic_link',                    to: "sessions#edit",          as: "magic_link"
   get     '/map',                           to: "map#show"
+  get     '/map/:id',                       to: "map#show_with_id",       as: "map_with_id"
+  get     '/map/query/:query',              to: "map#show_with_query",    as: "map_with_query"
   get     'map_data/info_panel/:id',        to: "info_panel#create",      as: "info_panel",       defaults: {format: :turbo_stream}
   delete  'favorite_cultivar/:id',          to: "favorites#destroy",      as: "delete_favorite"
   post    'favorite_culitvar/:id',          to: "favorites#create",       as: "add_favorite"
