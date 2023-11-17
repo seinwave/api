aa7aad0a89e33e3c89bdbcdea95fefcfd0d0c65d
Matt Seidholz <mseidholz@gmail.com>
2023-11-17 12:50:41 -0500

    redirect to root_url upon url-bar submission

--- app/controllers/info_panel_controller.rb
+++ app/controllers/info_panel_controller.rb
@@ -6,13 +6,14 @@ class InfoPanelController < ApplicationController
   end
 
   def create
-    puts "CREATING INFO PANEL"
-    
-    binding.pry
-    @cultivar = Cultivar.find(params[:id])
-    @current_cultivar = @cultivar
-    respond_to do |format|
-      format.turbo_stream
+    if request.referer != root_url
+      redirect_to root_url
+    else
+      @cultivar = Cultivar.find(params[:id])
+      @current_cultivar = @cultivar
+      respond_to do |format|
+        format.turbo_stream
+      end
     end
   end
 
