9ff8716836cbfc5ddd4473cc974b22ded520fb60
Matt Seidholz <mseidholz@gmail.com>
2023-12-18 10:33:31 -0500

    use nokogiri to re-size svgs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
 
-  def show_svg(path)
-    File.open("app/assets/images/#{path}", "rb") do |file|
-      raw file.read
-    end
+def show_svg(path)
+  File.open("app/assets/images/#{path}", "rb") do |file|
+    doc = Nokogiri::XML(file)
+    svg = doc.at_css('svg')
+    svg['width'] = 80
+    svg['height'] = 80
+    raw doc.to_xml
   end
+end
   
 end
