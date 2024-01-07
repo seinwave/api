89afbddfbfcfbd20ed76b71822683e0dadaa0bfd
Matt Seidholz <mseidholz@gmail.com>
2024-01-07 09:21:25 -0500

    add tests for counts, favorites

--- test/models/cultivar_test.rb
+++ test/models/cultivar_test.rb
@@ -1,6 +1,17 @@
 require "test_helper"
 
 class CultivarTest < ActiveSupport::TestCase
+
+  test "should return a count of favorites for a cultivar" do
+    cultivar = cultivars(:lily)
+    assert_equal 1, cultivar.global_favorite_count
+  end
+
+  test 'should return a count of plants for a cultivar' do
+    cultivar = cultivars(:lily)
+    assert_equal 1, cultivar.global_plant_count
+  end
+  
   test "should return the first plant for a cultivar" do
     cultivar = cultivars(:lily)
     first_plant = plants(:plant_001)
