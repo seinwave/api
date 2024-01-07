dd83e3d989b8c988460e16648a0372252c6bb6a2
Matt Seidholz <mseidholz@gmail.com>
2024-01-07 09:17:54 -0500

    fix plant test

--- test/models/plant_test.rb
+++ test/models/plant_test.rb
@@ -4,6 +4,6 @@ class PlantTest < ActiveSupport::TestCase
   
   test "should return a plant's coordinates" do
     plant = plants(:plant_001)
-    assert_equal [plant.latitude, plant.longitude], plant.get_coordinates
+    assert_equal [ plant.longitude,plant.latitude], plant.get_coordinates
   end
 end
